7934e615e86343dac489b17461ce0a10ce314fa1
nevres <nevres_jahic@hotmail.com>
2013-12-19 12:07:22 +0100

    first commit

---
+++ src/githubtest/GithubTest.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubtest;
+
+/**
+ *
+ * @author Phoenix
+ */
+public class GithubTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("this is just ffor testng purpose");
+    }
+}
