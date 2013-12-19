91effc97ac0d54724f8c9ab7ed11546f6ba987f5
nevres <nevres_jahic@hotmail.com>
2013-12-19 12:09:23 +0100

    changed sth

--- src/githubtest/GithubTest.java
+++ src/githubtest/GithubTest.java
@@ -15,5 +15,6 @@ public class GithubTest {
      */
     public static void main(String[] args) {
         System.out.println("this is just ffor testng purpose");
+        System.out.println("To commit");
     }
 }
